ead971ebbb69dd1f76cc6dae86683d10076fe4d5
gaozuoy666 <1370004040@qq.com>
2017-10-17 19:04:59 +0800

    Create main.py

---
+++ hw01/main.py
@@ -0,0 +1,7 @@
+#右上三角格式输出九九乘法表
+for i in range(1,10):
+    for k in range(1,i):
+        print (end="       ")
+    for j in range(i,10):
+            print("%d*%d=%2d" % (i,j,i*j),end=" ")
+    print("")
